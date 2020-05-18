148d2f54758e32b50d90f955599f5cf2d4ccfd77
Jose <gccinane@gmail.com>
2020-05-18 18:12:33 -0300

    estilizacao completa

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import path from 'path';
+
 
 const server = express();
 
-server.listen(3000)
+server.use(express.static(path.join(__dirname, 'public')))
 
 server.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
+  res.redirect('/index.html')
 })
 
+
+server.listen(3000)
