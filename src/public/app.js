03f50e53f025ccafa43d6a2eb8ac556ab164ee7f
Jose <gccinane@gmail.com>
2020-05-19 11:38:05 -0300

    troca de cor do background

--- src/public/app.js
+++ src/public/app.js
@@ -5,28 +5,26 @@ window.addEventListener('load', start);
 
 function start(){
   rgb = Array.from(document.querySelectorAll('.color'))
-  rgbValuesDisplay = Array.from(document.querySelectorAll('.colorValues'))
+  rgbValuesDisplay = Array.from(document.querySelectorAll('.colorValue'))
+  console.log(rgbValuesDisplay[0].value)
   rgb.map(value => value.addEventListener('input', handleValue))
+ 
 }
 
 function handleValue(event) {
   switch(event.target.id){
     case 'red':
-      rgbValuesDisplay[0] = document.querySelector('#redValue');
       rgbValuesDisplay[0].value = `${String(event.target.value)}`
       break
     case 'green':
-      rgbValuesDisplay[1]= document.querySelector('#greenValue');
       rgbValuesDisplay[1].value = `${String(event.target.value)}`
       break
     case 'blue':
-      rgbValuesDisplay[2] = document.querySelector('#blueValue');
       rgbValuesDisplay[2].value =`${String(event.target.value)}`     
       break    
     default:
       return
   }
-
-  document.querySelector('.container').style.backgroundColor = `${rgbValuesDisplay[0]},${rgbValuesDisplay[0]},${rgbValuesDisplay[0]}`
+  document.querySelector('#container').style.backgroundColor = `rgb(${rgbValuesDisplay[0].value},${rgbValuesDisplay[1].value},${rgbValuesDisplay[2].value})`
 }
 
